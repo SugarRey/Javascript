b4b55f2ecd0977a17fb4f0e27236a99ca42c01e4
SugarRey <shugarprogram1998@gmail.com>
2022-09-16 19:58:01 -0500

    Clase del dom de javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafo = document.querySelector('.parrafo');
+const parrafoid = document.querySelector('#parrafo');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+   h1,
+   p,
+   parrafo,
+   parrafoid,
+   input
+})
